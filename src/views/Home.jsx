3b9f00e53ce506f6bf98f3a5d7597d2615a1dc75
ethanMedium <ethan.cho@themedium.io>
2022-10-20 18:53:23 +0900

    dashboard and other pages basic front

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -14,7 +14,7 @@ function Home({ authService }) {
   return (
     <>
       <header className="bg-white shadow">
-        <div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+        <div className="mx-auto max-w-7xl py-2 px-4 sm:px-6 lg:px-8">
           <h1 className="text-3xl font-bold tracking-tight text-gray-900">
             Dashboard
           </h1>
@@ -23,8 +23,186 @@ function Home({ authService }) {
       <main>
         <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
           {/* Replace with your content */}
-          <div className="px-4 py-6 sm:px-0">
-            <div className="h-96 rounded-lg border-4 border-dashed border-gray-200" />
+          <div class="relative grid grid-cols-1 gap-4 p-4 mb-2 border rounded-lg cursor-pointer bg-white shadow-lg hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <div class="relative flex gap-4">
+              <img
+                src="https://icons.iconarchive.com/icons/diversity-avatars/avatars/256/charlie-chaplin-icon.png"
+                class="relative rounded-lg -mb-4 bg-white border h-12 w-12"
+                alt=""
+                loading="lazy"
+              />
+              <div class="flex flex-col">
+                <div class="flex justify-between">
+                  <p class="relative text-l whitespace-nowrap truncate overflow-hidden">
+                    COMMENTOR
+                  </p>
+                </div>
+                <p class="text-gray-400 text-sm">20 April, 2022</p>
+              </div>
+              <p class="text-gray-500 text-sm word-break: break-all overflow-hidden">
+                Lorem ipsum dolor sit amet consectetur adipisicing el
+                it.asdfasdfkhasdkflksdhjflashdfklahsdfklqohsqrzkomrshqzokmsqrhozkqrmohzrqkmoak
+              </p>
+            </div>
+          </div>
+          <div class="relative grid grid-cols-1 gap-4 p-4 mb-2 border rounded-lg bg-white shadow-lg cursor-pointer bg-white shadow-lg hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <div class="relative flex gap-4">
+              <img
+                src="https://icons.iconarchive.com/icons/diversity-avatars/avatars/256/charlie-chaplin-icon.png"
+                class="relative rounded-lg -mb-4 bg-white border h-12 w-12"
+                alt=""
+                loading="lazy"
+              />
+              <div class="flex flex-col">
+                <div class="flex justify-between">
+                  <p class="relative text-l whitespace-nowrap truncate overflow-hidden">
+                    COMMENTOR
+                  </p>
+                </div>
+                <p class="text-gray-400 text-sm">20 April, 2022</p>
+              </div>
+              <p class="text-gray-500 text-sm word-break: break-all overflow-hidden">
+                Lorem ipsum dolor sit amet consectetur adipisicing el
+                it.asdfasdfkhasdkflksdhjflashdfklahsdfklqohsqrzkomrshqzokmsqrhozkqrmohzrqkmoak
+              </p>
+            </div>
+          </div>
+          <div class="relative grid grid-cols-1 gap-4 p-4 mb-2 border rounded-lg bg-white shadow-lg cursor-pointer bg-white shadow-lg hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <div class="relative flex gap-4">
+              <img
+                src="https://icons.iconarchive.com/icons/diversity-avatars/avatars/256/charlie-chaplin-icon.png"
+                class="relative rounded-lg -mb-4 bg-white border h-12 w-12"
+                alt=""
+                loading="lazy"
+              />
+              <div class="flex flex-col">
+                <div class="flex justify-between">
+                  <p class="relative text-l whitespace-nowrap truncate overflow-hidden">
+                    COMMENTOR
+                  </p>
+                </div>
+                <p class="text-gray-400 text-sm">20 April, 2022</p>
+              </div>
+              <p class="text-gray-500 text-sm word-break: break-all overflow-hidden">
+                Lorem ipsum dolor sit amet consectetur adipisicing el
+                it.asdfasdfkhasdkflksdhjflashdfklahsdfklqohsqrzkomrshqzokmsqrhozkqrmohzrqkmoak
+              </p>
+            </div>
+          </div>
+          <div class="relative grid grid-cols-1 gap-4 p-4 mb-2 border rounded-lg bg-white shadow-lg cursor-pointer bg-white shadow-lg hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <div class="relative flex gap-4">
+              <img
+                src="https://icons.iconarchive.com/icons/diversity-avatars/avatars/256/charlie-chaplin-icon.png"
+                class="relative rounded-lg -mb-4 bg-white border h-12 w-12"
+                alt=""
+                loading="lazy"
+              />
+              <div class="flex flex-col">
+                <div class="flex justify-between">
+                  <p class="relative text-l whitespace-nowrap truncate overflow-hidden">
+                    COMMENTOR
+                  </p>
+                </div>
+                <p class="text-gray-400 text-sm">20 April, 2022</p>
+              </div>
+              <p class="text-gray-500 text-sm word-break: break-all overflow-hidden">
+                Lorem ipsum dolor sit amet consectetur adipisicing el
+                it.asdfasdfkhasdkflksdhjflashdfklahsdfklqohsqrzkomrshqzokmsqrhozkqrmohzrqkmoak
+              </p>
+            </div>
+          </div>
+
+          <div class="flex flex-col items-center">
+            <span class="text-sm text-gray-700 dark:text-gray-400">
+              Showing{" "}
+              <span class="font-semibold text-gray-900 dark:text-white">1</span>{" "}
+              to{" "}
+              <span class="font-semibold text-gray-900 dark:text-white">
+                10
+              </span>{" "}
+              of{" "}
+              <span class="font-semibold text-gray-900 dark:text-white">
+                100
+              </span>{" "}
+              Entries
+            </span>
+            <div class="inline-flex mt-2 xs:mt-0">
+              <button class="py-2 px-4 text-sm font-medium text-white bg-gray-800 rounded-l hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                Prev
+              </button>
+              <button class="py-2 px-4 text-sm font-medium text-white bg-gray-800 rounded-r border-0 border-l border-gray-700 hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                Next
+              </button>
+            </div>
+          </div>
+
+          {/* 여기서 부터 포스트 */}
+
+          <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-3">
+            <a
+              href="#"
+              class="block p-6 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+            >
+              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Noteworthy technology acquisitions 2021
+              </h5>
+              <p class="font-normal text-gray-700 dark:text-gray-400">
+                Here are the biggest enterprise technology acquisitions of 2021
+                so far, in reverse chronological order.
+              </p>
+            </a>
+
+            <a
+              href="#"
+              class="block p-6 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+            >
+              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Noteworthy technology acquisitions 2021
+              </h5>
+              <p class="font-normal text-gray-700 dark:text-gray-400">
+                Here are the biggest enterprise technology acquisitions of 2021
+                so far, in reverse chronological order.
+              </p>
+            </a>
+
+            <a
+              href="#"
+              class="block p-6 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+            >
+              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Noteworthy technology acquisitions 2021
+              </h5>
+              <p class="font-normal text-gray-700 dark:text-gray-400">
+                Here are the biggest enterprise technology acquisitions of 2021
+                so far, in reverse chronological order.
+              </p>
+            </a>
+
+            <a
+              href="#"
+              class="block p-6 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+            >
+              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Noteworthy technology acquisitions 2021
+              </h5>
+              <p class="font-normal text-gray-700 dark:text-gray-400">
+                Here are the biggest enterprise technology acquisitions of 2021
+                so far, in reverse chronological order.
+              </p>
+            </a>
+
+            <a
+              href="#"
+              class="block p-6 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+            >
+              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Noteworthy technology acquisitions 2021
+              </h5>
+              <p class="font-normal text-gray-700 dark:text-gray-400">
+                Here are the biggest enterprise technology acquisitions of 2021
+                so far, in reverse chronological order.
+              </p>
+            </a>
           </div>
           {/* /End replace */}
         </div>
--- src/views/Report.jsx
+++ src/views/Report.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import DateRangePicker from "flowbite-datepicker/DateRangePicker";
 
 function Report({ authService }) {
   const navigate = useNavigate();
@@ -23,9 +24,139 @@ function Report({ authService }) {
       <main>
         <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
           {/* Replace with your content */}
-          <div className="px-4 py-6 sm:px-0">
-            <div className="h-96 rounded-lg border-4 border-dashed border-gray-200" />
+          <div date-rangepicker class="flex items-center mb-4">
+            <div class="relative">
+              <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                <svg
+                  aria-hidden="true"
+                  class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </div>
+              <input
+                name="start"
+                type="text"
+                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Select date start"
+              />
+            </div>
+            <span class="mx-4 text-gray-500">to</span>
+            <div class="relative">
+              <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                <svg
+                  aria-hidden="true"
+                  class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </div>
+              <input
+                name="end"
+                type="text"
+                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Select date end"
+              />
+            </div>
           </div>
+          <div class="overflow-x-auto relative shadow-md sm:rounded-lg">
+            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+              <thead class="text-xs text-gray-700 uppercase dark:text-gray-400">
+                <tr>
+                  <th scope="col" class="py-3 px-6 bg-gray-50 dark:bg-gray-800">
+                    Product name
+                  </th>
+                  <th scope="col" class="py-3 px-6">
+                    Color
+                  </th>
+                  <th scope="col" class="py-3 px-6 bg-gray-50 dark:bg-gray-800">
+                    Category
+                  </th>
+                  <th scope="col" class="py-3 px-6">
+                    Price
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr class="border-b border-gray-200 dark:border-gray-700">
+                  <th
+                    scope="row"
+                    class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800"
+                  >
+                    Apple MacBook Pro 17"
+                  </th>
+                  <td class="py-4 px-6">Sliver</td>
+                  <td class="py-4 px-6 bg-gray-50 dark:bg-gray-800">Laptop</td>
+                  <td class="py-4 px-6">$2999</td>
+                </tr>
+                <tr class="border-b border-gray-200 dark:border-gray-700">
+                  <th
+                    scope="row"
+                    class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800"
+                  >
+                    Microsoft Surface Pro
+                  </th>
+                  <td class="py-4 px-6">White</td>
+                  <td class="py-4 px-6 bg-gray-50 dark:bg-gray-800">
+                    Laptop PC
+                  </td>
+                  <td class="py-4 px-6">$1999</td>
+                </tr>
+                <tr class="border-b border-gray-200 dark:border-gray-700">
+                  <th
+                    scope="row"
+                    class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800"
+                  >
+                    Magic Mouse 2
+                  </th>
+                  <td class="py-4 px-6">Black</td>
+                  <td class="py-4 px-6 bg-gray-50 dark:bg-gray-800">
+                    Accessories
+                  </td>
+                  <td class="py-4 px-6">$99</td>
+                </tr>
+                <tr class="border-b border-gray-200 dark:border-gray-700">
+                  <th
+                    scope="row"
+                    class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800"
+                  >
+                    Google Pixel Phone
+                  </th>
+                  <td class="py-4 px-6">Gray</td>
+                  <td class="py-4 px-6 bg-gray-50 dark:bg-gray-800">Phone</td>
+                  <td class="py-4 px-6">$799</td>
+                </tr>
+                <tr>
+                  <th
+                    scope="row"
+                    class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800"
+                  >
+                    Apple Watch 5
+                  </th>
+                  <td class="py-4 px-6">Red</td>
+                  <td class="py-4 px-6 bg-gray-50 dark:bg-gray-800">
+                    Wearables
+                  </td>
+                  <td class="py-4 px-6">$999</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+
           {/* /End replace */}
         </div>
       </main>
