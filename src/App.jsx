a50f19a07f010adb55172c6371076f9e4c0d994a
ethanMedium <ethan.cho@themedium.io>
2022-10-20 10:06:51 +0900

    routes are connected

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+// react
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+// src
+import Header from "./components/layout/Header";
+import Home from "./views/Home";
+import Monitor from "./views/Monitor";
+import Login from "./views/Login";
+import Report from "./views/Report";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/monitor" element={<Monitor />} />
+        <Route path="/report" element={<Report />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
